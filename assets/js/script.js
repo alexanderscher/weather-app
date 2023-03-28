424ee79b6f4ceb091c10f0538902b2434dd19e74
alexanderscher <alexcscher@gmail.com>
2023-03-27 17:52:56 -0700

    started on forcast js and css layout. started on local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@ var apiKey = "1791efc900804a415be233a50a04e460";
 var city = "Los Angeles";
 var cityEndpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
 
+var cit = document.querySelector("#city");
+var weath = document.querySelector("#weather");
+var tem = document.querySelector("#temperature");
+var humid = document.querySelector("#humidity");
+var win = document.querySelector("#wind-speed");
+var forecast = document.querySelector(".forecast");
+
 fetch(cityEndpoint)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data[0].lat);
-    console.log(data[0].lon);
-
     var lon = data[0].lon;
     var lat = data[0].lat;
     var todayEndpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
@@ -20,11 +24,26 @@ fetch(cityEndpoint)
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-        var temp = data.main.temp;
+        var kelvin = data.main.temp;
+        var temp = Math.round((kelvin - 273.15) * 9) / 5 + 32;
+        var city = `${data.name}, ${data.sys.country}`;
         var humidity = data.main.humidity;
-        var wind = data.wind.speed;
-        console.log(temp, humidity, wind);
+        var wind = Math.round(data.wind.speed * 2.2369362920544);
+        var weatherType = data.weather[0].main;
+
+        cit.innerHTML = city;
+        weath.innerHTML = weatherType;
+        tem.innerHTML = temp + "°F";
+        humid.innerHTML = humidity + "%";
+        win.innerHTML = wind + "mph";
+
+        todayWeather = {
+          city: city,
+          temp: temp,
+          humidity: humidity,
+          wind: wind,
+        };
+        localStorage.setItem("today", JSON.stringify(todayWeather));
       });
 
     fetch(forecastEndpoint)
@@ -33,21 +52,56 @@ fetch(cityEndpoint)
       })
       .then(function (data) {
         var checkedDates = [];
+        var storage = [];
+
         data.list.forEach(function (x) {
           var dates = x.dt_txt.split(" ");
-          var temp = x.main.temp;
+          var kelvin = x.main.temp;
+          var temp = Math.round((kelvin - 273.15) * 9) / 5 + 32;
           var humidity = x.main.humidity;
-          var wind = x.wind.speed;
+          var wind = Math.round(x.wind.speed * 2.2369362920544);
+          var weatherType = x.weather[0].main;
 
           if (checkedDates.includes(dates[0])) {
           } else {
             checkedDates.push(dates[0]);
-            console.log(dates[0], temp, humidity, wind);
+
+            forecastStorage = {
+              date: dates[0],
+              temp: temp,
+              humidity: humidity,
+              wind: wind,
+              weatherType: weatherType,
+            };
+
+            storage.push(forecastStorage);
+
+            var outDiv = document.createElement("div");
+            var inDiv = document.createElement("div");
+            outDiv.className = "forecast-data";
+
+            var dateF = document.createElement("p");
+            var tempF = document.createElement("p");
+            var humF = document.createElement("p");
+            var windF = document.createElement("p");
+            var weathF = document.createElement("p");
+
+            dateF.innerHTML = "Date: " + dates[0];
+            weathF.innerHTML = "Weather: " + weatherType;
+            tempF.innerHTML = "Temp: " + temp + "°F";
+            humF.innerHTML = "Humidity: " + humidity + "%";
+            windF.innerHTML = "Wind-Speed: " + wind + "mph";
+
+            inDiv.append(dateF);
+            inDiv.append(weathF);
+            inDiv.append(tempF);
+            inDiv.append(humF);
+            inDiv.append(windF);
+
+            outDiv.append(inDiv);
+            forecast.append(outDiv);
           }
+          localStorage.setItem("forecast", JSON.stringify(storage));
         });
       });
   });
-
-// temp;
-// wind;
-// humidity;

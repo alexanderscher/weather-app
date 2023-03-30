bea7f673cd641182743d24db3a097e0f693ca941
alexanderscher <alexcscher@gmail.com>
2023-03-30 16:56:26 -0700

    past searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,10 @@ var button = document.querySelector("button");
 var input = document.querySelector("input");
 var current = document.querySelector(".current-weather");
 var weatherIcon = document.getElementById("weather-icon");
+var recent = document.querySelector(".recent-searches");
+var pastCities = document.querySelector(".past-cities");
+
+var past = [];
 
 function dayAppend(city, today, weatherType, temp, humidity, wind) {
   cit.innerHTML = city;
@@ -21,7 +25,6 @@ function dayAppend(city, today, weatherType, temp, humidity, wind) {
   win.innerHTML = wind + "mph";
 
   var type = weatherType.toLowerCase();
-  console.log(type);
   weatherIcon.className = `wi wi-day-${weatherType.toLowerCase()}`;
 }
 
@@ -53,11 +56,22 @@ function forecastAppend(temp, humidity, wind, weatherType, reformattedDate) {
   var i = document.createElement("i");
   i.id = "weather-icon";
 
-  var type = weatherType.toLowerCase();
   i.className = `wi wi-day-${weatherType.toLowerCase()}`;
   icon.append(i);
   outDiv.append(icon);
   forecast.append(outDiv);
+}
+
+function pastSearch(city) {
+  past.push(city);
+  var p = document.createElement("button");
+  p.className = "past-cities";
+  p.innerHTML = city;
+  recent.append(p);
+
+  p.addEventListener("click", () => {
+    getCity(city);
+  });
 }
 
 function getCity(input) {
@@ -84,6 +98,8 @@ function getCity(input) {
           var weatherType = data.weather[0].main;
           var today = dayjs().format("MM-DD-YYYY");
 
+          pastSearch(city);
+
           todayWeather = {
             date: today,
             city: city,
@@ -92,6 +108,8 @@ function getCity(input) {
             wind: wind,
             weatherType: weatherType,
           };
+
+          localStorage.setItem("past", JSON.stringify(past));
           localStorage.setItem("today", JSON.stringify(todayWeather));
 
           dayAppend(city, today, weatherType, temp, humidity, wind);
@@ -120,11 +138,9 @@ function getCity(input) {
             if (checkedDates.includes(dates[0])) {
             } else {
               if (index < 5) {
-                console.log(index);
                 index++;
                 checkedDates.push(dates[0]);
                 var date = dayjs(dates[0]);
-                console.log(date);
                 var reformattedDate = date.format("MM-DD-YYYY");
                 forecastAppend(
                   temp,
@@ -153,18 +169,28 @@ function getCity(input) {
 
 function getStorage() {
   todayStorage = JSON.parse(localStorage.getItem("today"));
-  dayAppend(
-    todayStorage.city,
-    todayStorage.date,
-    todayStorage.weatherType,
-    todayStorage.temp,
-    todayStorage.humidity,
-    todayStorage.wind
-  );
+  if (todayStorage !== null) {
+    dayAppend(
+      todayStorage.city,
+      todayStorage.date,
+      todayStorage.weatherType,
+      todayStorage.temp,
+      todayStorage.humidity,
+      todayStorage.wind
+    );
+  }
   forecastStorage = JSON.parse(localStorage.getItem("forecast"));
-  forecastStorage.forEach(function (x) {
-    forecastAppend(x.temp, x.humidity, x.wind, x.weatherType, x.date, x.city);
-  });
+  if (forecastStorage !== null) {
+    forecastStorage.forEach(function (x) {
+      forecastAppend(x.temp, x.humidity, x.wind, x.weatherType, x.date, x.city);
+    });
+  }
+  pastSt = JSON.parse(localStorage.getItem("past"));
+  if (pastSt !== null) {
+    pastSt.forEach(function (x) {
+      pastSearch(x);
+    });
+  }
 }
 
 function search(event) {
